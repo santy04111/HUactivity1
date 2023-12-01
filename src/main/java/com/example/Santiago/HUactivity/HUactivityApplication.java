8372d0852b37e5019a9a32308e7d51a23d0ce81f
santy04111 <116660710+santy04111@users.noreply.github.com>
2023-12-01 08:38:59 -0500

    Initial commit

---
+++ src/main/java/com/example/Santiago/HUactivity/HUactivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Santiago.HUactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HUactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HUactivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Santiago/HUactivity/HUactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Santiago.HUactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HUactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
